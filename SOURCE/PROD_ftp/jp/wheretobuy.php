2b65b9934eb1bcf280e9c0dedaedd383c9cc8fdb
Kyo Yamada <hysteric.madam@gmail.com>
2019-01-02 18:37:27 +0900

    replace files for FTP

--- SOURCE/PROD_ftp/jp/wheretobuy.php
+++ SOURCE/PROD_ftp/jp/wheretobuy.php
@@ -8,259 +8,344 @@
     </div>
   </div>
 
-  <div id="shoplist">
-    <table>
-      <tr>
-      <th>
-      <h3>アジア</h3></th>
-        <td>
-        <h4>香港</h4></td>
-        <td><h5>
-        Audio Exotics</h5>
-        <a href="http://www.audioexotics.hk/" target="_blank">
-        http://www.audioexotics.hk</a><br />
-        <br />
-        Contact: Mr. Jlam<br />
-        E-mail: info@audioexotics.com<br />
-        Phone: (852) 9181-8291
-        </td>
-      </tr>
-      <tr>
-      <th></th>
-        <td>
-        <h4>日本</h4></td>
-        <td><h5>
-        Zanden Audio Systems</h5>
-        <a href="http://www.zandenaudio.com/jp/" target="_blank">
-        http://www.zandenaudio.com/jp/</a><br />
-        <br />
-        596-0845<br />
-        大阪府岸和田市阿間河滝町1558-1<br />
-        <br />
-        Phone: 072-479-5007
-        </td>
-      </tr>
-      <tr>
-      <th></th>
-        <td>
-        <h4>マレーシア</h4></td>
-        <td><h5>
-        Sound Concepts</h5>
-        <br />
-        Unit 18-25B, 25th Floor, Gurney Tower,<br />
-        18 Persiaran Gurney,<br />
-        10250 Penang,<br />
-        Malaysia.<br />
-        <br />
-        Contact: Mr. Andre Sim<br />
-        E-mail: andresim@soundconcepts.biz<br />
-        Phone: +6 012 429 6101
-        </td>
-      </tr>
-      <tr>
-      <th></th>
-        <td>
-        <h4>タイ</h4></td>
-        <td><h5>
-        Grand Audio &amp; Visual</h5>
-        <a href="http://www.grand-av.com/" target="_blank">
-        http://www.grand-av.com</a><br />
-        <br />
-        138 Soi Sukhumvit 62 Yaek1 , Sukhumvit Rd., Bangchak,<br />
-        Prakanong , Bangkok 10260<br />
-        <br />
-        Contact: Mr. Boonyarat Bojarus<br />
-        E-mail: boonyarat@btp.co.th<br />
-        Phone: 66 (0) 2 7416137
-        </td>
-      </tr>
-      <tr>
-      <th></th>
-        <td>
-        <h4>ベトナム</h4></td>
-        <td><h5>
-        Eland Audio</h5>
-        <a href="http://www.elandaudio.com/" target="_blank">
-        http://www.elandaudio.com</a><br />
-        <br />
-        6A Road 3 Binh An Ward<br />
-        District 2 Hochiminh City<br />
-        Vietnam<br />
-        <br />
-        E-mail: info@elandaudio.com<br />
-        Phone: +84 969 969 969
-        </td>
-      </tr>
-      <tr>
-      <th>
-      <h3>ヨーロッパ</h3></th>
-        <td>
-        <h4>イギリス</h4></td>
-        <td><h5>
-        AUDIOFREAKS</h5>
-        <a href="http://www.audiofreaks.co.uk/" target="_blank">
-        http://www.audiofreaks.co.uk</a><br />
-        <br />
-        15 Link Way, Ham,<br />
-        Richmond, Surrey, TW10 7QT<br />
-        United Kingdom<br />
-        <br />
-        E-mail: info1@audiofreaks.co.uk<br />
-        Phone: (+44) 208 948 4153<br />
-        Fax: (+44) 208 948 4250
-        </td>
-      </tr>
-      <tr>
-      <th></th>
-        <td>
-        <h4>ベネルクス</h4></td>
-        <td><h5>
-        Harmony Audio import</h5>
-        <a href="http://harmonyaudioimport.com/" target="_blank">
-        http://harmonyaudioimport.com</a><br />
-        <br />
-        Graaf Florislaan 6, 2553DW. The Hague.<br />
-        Netherlands<br />
-        <br />
-        Contact: Mr. Kees Wijnberger<br />
-        Phone: +31 (0) 646312235<br />
-        Phone: +31 (0) 653354337
-        </td>
-      </tr>
-      <tr>
-      <th></th>
-        <td>
-        <h4>イタリア</h4></td>
-        <td><h5>
-        AUDIO POINT ITALIA s.r.l.</h5>
-        <a href="http://www.audiopointitalia.it/" target="_blank">
-        http://www.audiopointitalia.it</a><br />
-        <a href="http://www.audiopointitalia.com/" target="_blank">
-        http://www.audiopointitalia.com</a><br />
-        <br />
-        VIA MOLLICA, 63<br />
-        95021 ACI CASTELLO<br />
-        (CT) ITALY<br />
-        <br />
-        Contact: Salvatore Giardina<br />
-        E-mail: info@audiopointitalia.it<br />
-        PHONE: +39 (0)95 272 601
-        </td>
-      </tr>
-      <tr>
-      <th></th>
-        <td>
-        <h4>スロベニア</h4></td>
-        <td><h5>
-        KUZMA LTD</h5>
-        <a href="http://www.katltd.si/" target="_blank">
-        http://www.katltd.si/</a><br />
-        <br />
-        HOTEMAZE 17A<br />
-        SI-4205 PREDDVOR<br />
-        SLOVENIA<br />
-        <br />
-        E-mail: kat.trgovina@gmail.com<br />
-        Phone: +386 068 650 802
-        </td>
-      </tr>
-      <tr>
-      <th></th>
-        <td>
-        <h4>セルビア</h4></td>
-        <td><h5>
-        BM</h5>
-        <a href="http://www.bm.rs/" target="_blank">
-        http://www.bm.rs</a><br />
-        <br />
-        Vojvode Milenka 9, Beograd<br />
-        Serbia<br />
-        <br />
-        E-mail: office@bm.rs<br />
-        Phone: (+381) 011/3618-000
-        </td>
-      </tr>
-      <tr>
-      <th></th>
-        <td>
-        <h4>ギリシャ</h4></td>
-        <td><h5>
-        Audio Temple</h5>
-        14 Argostoliou st.<br />
-        11362 Athens, Greece<br />
-        <br />
-        E-mail: info@audiotemple.gr<br />
-        Phone: 210 64 29655<br />
-        Fax: 210 64 12192
-        </td>
-      </tr>
-      <tr>
-      <th></th>
-        <td>
-        <h4>トルコ</h4></td>
-        <td><h5>
-        Extreme Audio</h5>
-        <a href="http://www.extreme-audio.com/" target="_blank">
-        http://www.extreme-audio.com</a><br />
-        <br />
-        Eskiuskudar Icerenkoy Yolu 12/2<br />
-        Icerenkoy 34752 Istanbul Turkey<br />
-        <br />
-        E-mail: info@extreme-audio.com<br />
-        Phone: 0216-574-38-77<br />
-        Fax: 0216-574-38-46
-        </td>
-      </tr>
-      <tr>
-      <th></th>
-        <td>
-        <h4>スカンジナビア</h4>
-        <em>・スウェーデン</em><br />
-        <em>・デンマーク</em><br />
-        <em>・ノルウェー</em><br />
-        <em>・フィンランド</em></td>
-        <td><h5>
-        Audiotrade</h5>
-        <a href="http://www.audiotrade.se/" target="_blank">
-        http://www.audiotrade.se</a><br />
-        <br />
-        E-mail: mikael@audiotrade.se<br />
-        </td>
-      </tr>
-      <tr>
-      <th>
-      <h3>北米</h3></th>
-        <td>
-        <h4>北米地域</h4>
-        <em>・アメリカ</em><br />
-        <em>・カナダ</em><br />
-        <em>・メキシコ</em><br />
-        <em>・カリブ地域</em></td>
-        <td><h5>
-        Zanden Audio North America</h5>
-        <a href="http://www.zanden-usa.com/" target="_blank">
-        http://www.zanden-usa.com</a><br />
-        <br />
-        Contact: Mr. Eric Pheils<br />
-        E-mail: sales@zanden-usa.com
-        </td>
-      </tr>
-      <tr>
-      <th>
-      <h3>南米</h3></th>
-        <td>
-        <h4>ブラジル</h4></td>
-        <td><h5>
-        Performance A/V Systems Ltda.</h5>
-        Rua Prof João Mendes França, 175<br />
-        São Paulo - SP<br />
-        Brasil<br />
-        <br />
-        E-mail: Jmlab@terra.com.br<br />
-        Phone: 55 +11 5103-0033
-        </td>
-      </tr>
-    </table>
+  <div class="wrapper shop-list-wrapper">
+    <h2>国内</h2>
+    <div class="shop-list">
+      <table>
+        <tr>
+        <th>
+        <h3>関東</h3></th>
+          <td>
+          <h4>東京</h4></td>
+          <td><h5>
+          Maestro Garage　マエストロ・ガレージ</h5>
+          <a href="http://www.maestrogarage.com/" target="_blank">
+          http://www.maestrogarage.com</a><br />
+          <br />
+          〒167-0051 東京都杉並区荻窪5-16-14 102<br />
+          TEL: 03-6383-5786 / FAX: 03-6383-5786<br />
+          E-mail: maestro@tk9.so-net.ne.jp<br />
+          展示機: アクセサリーのみ
+          </td>
+        </tr>
+        <tr>
+        <th>
+        <h3>関西</h3></th>
+          <td>
+          <h4>大阪</h4></td>
+          <td><h5>
+          株式会社シマムセン</h5>
+          <a href="https://www.shimamusen.co.jp/" target="_blank">
+          https://www.shimamusen.co.jp</a><br />
+          <br />
+          〒556-0005 大阪府大阪市浪速区日本橋4丁目8-11<br />
+          TEL: 06-6632-2851<br />
+          展示機: Modern Collection
+          </td>
+        </tr>
+        <tr>
+        <th></th>
+          <td></td>
+          <td><h5>
+          あなろぐふぁーむ</h5>
+          <a href="http://analog-farm.sblo.jp/" target="_blank">
+          http://analog-farm.sblo.jp</a><br />
+          <br />
+          〒596-0845 大阪府岸和田市阿間河滝町1643番地<br />
+          TEL: 090-4306-8989<br />
+          展示機: Modern Collection
+          </td>
+        </tr>
+        <tr>
+        <th></th>
+          <td></td>
+          <td><h5>
+          Luxury Audio ラグジュアリーオーディオ</h5>
+          <a href="http://www.luxuryaudio.org/" target="_blank">
+          http://www.luxuryaudio.org</a><br />
+          <br />
+          〒542-0081 大阪府大阪市中央区南船場1丁目10番12号<br />
+          Towaローゼンビル長堀201号室<br />
+          TEL: 06-6210-4221<br />
+          E-mail: info@luxuryaudio.org<br />
+          展示機: Modern Collection, Classic Collection（一部）
+          </td>
+        </tr>
+        <tr>
+        <th></th>
+          <td>
+          <h4>京都</h4></td>
+          <td><h5>
+          オーディオアミーゴ京都</h5>
+          <a href="http://audio-amigo.com/" target="_blank">
+          http://audio-amigo.com/</a><br />
+          <br />
+          〒614-8374 京都府八幡市男山石城4-18<br />
+          TEL: 075-981-1800<br />
+          E-mail: kyoto@audio-amigo.com<br />
+          展示機: Modern Collection, Classic Collection（一部）
+          </td>
+        </tr>
+        <tr>
+        <th>
+        <h3>九州</h3></th>
+          <td>
+          <h4>福岡</h4></td>
+          <td><h5>
+          吉田苑</h5>
+          <a href="http://www.yoshidaen.com/" target="_blank">
+          http://www.yoshidaen.com</a><br />
+          <br />
+          〒810-0015 福岡県福岡市中央区那の川2丁目9-30<br />
+          TEL: 092-522-5663 / FAX: 092-522-5812<br />
+          展示機: Modern Collection
+          </td>
+        </tr>
+      </table>
+    </div>
+  </div>
+
+  <div class="wrapper shop-list-wrapper">
+    <h2>海外</h2>
+    <div class="shop-list">
+      <table>
+        <tr>
+        <th>
+        <h3>アジア</h3></th>
+          <td>
+          <h4>香港</h4></td>
+          <td><h5>
+          Audio Exotics</h5>
+          <a href="http://www.audioexotics.hk/" target="_blank">
+          http://www.audioexotics.hk</a><br />
+          <br />
+          Contact: Mr. Jlam<br />
+          E-mail: info@audioexotics.com<br />
+          Phone: (852) 9181-8291
+          </td>
+        </tr>
+        <tr>
+        <th></th>
+          <td>
+          <h4>マレーシア</h4></td>
+          <td><h5>
+          Sound Concepts</h5>
+          <br />
+          Unit 18-25B, 25th Floor, Gurney Tower,<br />
+          18 Persiaran Gurney,<br />
+          10250 Penang,<br />
+          Malaysia.<br />
+          <br />
+          Contact: Mr. Andre Sim<br />
+          E-mail: andresim@soundconcepts.biz<br />
+          Phone: +6 012 429 6101
+          </td>
+        </tr>
+        <tr>
+        <th></th>
+          <td>
+          <h4>タイ</h4></td>
+          <td><h5>
+          Grand Audio &amp; Visual</h5>
+          <a href="http://www.grand-av.com/" target="_blank">
+          http://www.grand-av.com</a><br />
+          <br />
+          138 Soi Sukhumvit 62 Yaek1 , Sukhumvit Rd., Bangchak,<br />
+          Prakanong , Bangkok 10260<br />
+          <br />
+          Contact: Mr. Boonyarat Bojarus<br />
+          E-mail: boonyarat@btp.co.th<br />
+          Phone: 66 (0) 2 7416137
+          </td>
+        </tr>
+        <tr>
+        <th></th>
+          <td>
+          <h4>ベトナム</h4></td>
+          <td><h5>
+          Eland Audio</h5>
+          <a href="http://www.elandaudio.com/" target="_blank">
+          http://www.elandaudio.com</a><br />
+          <br />
+          6A Road 3 Binh An Ward<br />
+          District 2 Hochiminh City<br />
+          Vietnam<br />
+          <br />
+          E-mail: info@elandaudio.com<br />
+          Phone: +84 969 969 969
+          </td>
+        </tr>
+        <tr>
+        <th>
+        <h3>ヨーロッパ</h3></th>
+          <td>
+          <h4>イギリス</h4></td>
+          <td><h5>
+          AUDIOFREAKS</h5>
+          <a href="http://www.audiofreaks.co.uk/" target="_blank">
+          http://www.audiofreaks.co.uk</a><br />
+          <br />
+          15 Link Way, Ham,<br />
+          Richmond, Surrey, TW10 7QT<br />
+          United Kingdom<br />
+          <br />
+          E-mail: info1@audiofreaks.co.uk<br />
+          Phone: (+44) 208 948 4153<br />
+          Fax: (+44) 208 948 4250
+          </td>
+        </tr>
+        <tr>
+        <th></th>
+          <td>
+          <h4>ベネルクス</h4></td>
+          <td><h5>
+          Harmony Audio import</h5>
+          <a href="http://harmonyaudioimport.com/" target="_blank">
+          http://harmonyaudioimport.com</a><br />
+          <br />
+          Graaf Florislaan 6, 2553DW. The Hague.<br />
+          Netherlands<br />
+          <br />
+          Contact: Mr. Kees Wijnberger<br />
+          Phone: +31 (0) 646312235<br />
+          Phone: +31 (0) 653354337
+          </td>
+        </tr>
+        <tr>
+        <th></th>
+          <td>
+          <h4>イタリア</h4></td>
+          <td><h5>
+          AUDIO POINT ITALIA s.r.l.</h5>
+          <a href="http://www.audiopointitalia.it/" target="_blank">
+          http://www.audiopointitalia.it</a><br />
+          <a href="http://www.audiopointitalia.com/" target="_blank">
+          http://www.audiopointitalia.com</a><br />
+          <br />
+          VIA MOLLICA, 63<br />
+          95021 ACI CASTELLO<br />
+          (CT) ITALY<br />
+          <br />
+          Contact: Salvatore Giardina<br />
+          E-mail: info@audiopointitalia.it<br />
+          PHONE: +39 (0)95 272 601
+          </td>
+        </tr>
+        <tr>
+        <th></th>
+          <td>
+          <h4>スロベニア</h4></td>
+          <td><h5>
+          KUZMA LTD</h5>
+          <a href="http://www.katltd.si/" target="_blank">
+          http://www.katltd.si/</a><br />
+          <br />
+          HOTEMAZE 17A<br />
+          SI-4205 PREDDVOR<br />
+          SLOVENIA<br />
+          <br />
+          E-mail: kat.trgovina@gmail.com<br />
+          Phone: +386 068 650 802
+          </td>
+        </tr>
+        <tr>
+        <th></th>
+          <td>
+          <h4>セルビア</h4></td>
+          <td><h5>
+          BM</h5>
+          <a href="http://www.bm.rs/" target="_blank">
+          http://www.bm.rs</a><br />
+          <br />
+          Vojvode Milenka 9, Beograd<br />
+          Serbia<br />
+          <br />
+          E-mail: office@bm.rs<br />
+          Phone: (+381) 011/3618-000
+          </td>
+        </tr>
+        <tr>
+        <th></th>
+          <td>
+          <h4>ギリシャ</h4></td>
+          <td><h5>
+          Audio Temple</h5>
+          14 Argostoliou st.<br />
+          11362 Athens, Greece<br />
+          <br />
+          E-mail: info@audiotemple.gr<br />
+          Phone: 210 64 29655<br />
+          Fax: 210 64 12192
+          </td>
+        </tr>
+        <tr>
+        <th></th>
+          <td>
+          <h4>トルコ</h4></td>
+          <td><h5>
+          Extreme Audio</h5>
+          <a href="http://www.extreme-audio.com/" target="_blank">
+          http://www.extreme-audio.com</a><br />
+          <br />
+          Eskiuskudar Icerenkoy Yolu 12/2<br />
+          Icerenkoy 34752 Istanbul Turkey<br />
+          <br />
+          E-mail: info@extreme-audio.com<br />
+          Phone: 0216-574-38-77<br />
+          Fax: 0216-574-38-46
+          </td>
+        </tr>
+        <tr>
+        <th></th>
+          <td>
+          <h4>スカンジナビア</h4>
+          <em>・スウェーデン</em><br />
+          <em>・デンマーク</em><br />
+          <em>・ノルウェー</em><br />
+          <em>・フィンランド</em></td>
+          <td><h5>
+          Audiotrade</h5>
+          <a href="http://www.audiotrade.se/" target="_blank">
+          http://www.audiotrade.se</a><br />
+          <br />
+          E-mail: mikael@audiotrade.se<br />
+          </td>
+        </tr>
+        <tr>
+        <th>
+        <h3>北米</h3></th>
+          <td>
+          <h4>北米地域</h4>
+          <em>・アメリカ</em><br />
+          <em>・カナダ</em><br />
+          <em>・メキシコ</em><br />
+          <em>・カリブ地域</em></td>
+          <td><h5>
+          Zanden Audio North America</h5>
+          <a href="http://www.zanden-usa.com/" target="_blank">
+          http://www.zanden-usa.com</a><br />
+          <br />
+          Contact: Mr. Eric Pheils<br />
+          E-mail: sales@zanden-usa.com
+          </td>
+        </tr>
+        <tr>
+        <th>
+        <h3>南米</h3></th>
+          <td>
+          <h4>ブラジル</h4></td>
+          <td><h5>
+          Performance A/V Systems Ltda.</h5>
+          Rua Prof João Mendes França, 175<br />
+          São Paulo - SP<br />
+          Brasil<br />
+          <br />
+          E-mail: Jmlab@terra.com.br<br />
+          Phone: 55 +11 5103-0033
+          </td>
+        </tr>
+      </table>
+    </div>
   </div>
 
   </div>
